0012b73ad108b635e3b8c5701be96e1af4651b5e
Catherine <tuhongj@cn.ibm.com>
2019-05-10 13:10:58 +0800

    Add attach and detach volumes
    Signed-off-by: Catherine <tuhongj@cn.ibm.com>

---
+++ scripts/backend/attach_volume.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd `dirname $0`
+source ../cloudrc
+
+[ $# -lt 3 ] && echo "$0 <vm_ID> <volume_ID> <volume_path>" && exit -1
+
+vm_ID=inst-$1
+vol_ID=$2
+vol_path=$3
+vol_xml=$xml_dir/$vm_ID/disk-${vol_ID}.xml
+virsh attach-device $vm_ID $vol_xml --config --persistent
+if [ $? -eq 0 ]; then
+    echo "|:-COMMAND-:| `basename $0` $vm_ID $vol_ID $device"
+else
+    echo "|:-COMMAND-:| `basename $0` '' '$vol_ID' ''"
+fi
+vm_xml=$xml_dir/$vm_ID/$vm_ID.xml
+virsh dumpxml --security-info $vm_ID 2>/dev/null | sed "s/autoport='yes'/autoport='no'/g" > $vm_xml.dump && mv -f $vm_xml.dump $vm_xml
