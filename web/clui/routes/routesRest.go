ae6423498639754241b575b0fe8fa8204daf2d54
spark <spark@sparkmpb.cn.ibm.com>
2019-06-04 17:38:04 +0800

    support openstack identity API

---
+++ web/clui/routes/routesRest.go
@@ -0,0 +1,49 @@
+/*
+Copyright <holder> All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package routes
+
+import (
+	"fmt"
+	"html/template"
+	"strings"
+
+	"github.com/spf13/viper"
+	"gopkg.in/macaron.v1"
+)
+
+var (
+	resourceEndpoints = map[string]string{
+		"volume":   `/volume/v3`,
+		"compute":  `/compute/v2.1`,
+		"image":    `/image`,
+		"network":  `/network`,
+		"identity": `/identity`,
+	}
+)
+
+func RunRest() (err error) {
+	Rest().Run(runArgs("rest.listen")...)
+	return
+}
+
+func Rest() (m *macaron.Macaron) {
+	m = macaron.Classic()
+	m.Use(macaron.Renderer(
+		macaron.RenderOptions{
+			Funcs: []template.FuncMap{
+				template.FuncMap{
+					"GetString": viper.GetString,
+					"Title":     func(v interface{}) string { return strings.Title(fmt.Sprint(v)) },
+				},
+			},
+		},
+	))
+	//	m.Use(macaron.Renderer())
+	m.Get(resourceEndpoints["identity"], versionInstance.ListVersion)
+	m.Post("/identity/v3/auth/tokens", tokenInstance.IssueTokenByPasswd)
+	return
+}
