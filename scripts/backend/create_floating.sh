dcc9a1962a87f3e5a452c0c9d9a9db53e9062be2
Catherine <tuhongj@cn.ibm.com>
2019-06-06 00:42:59 +0000

    Fix floating ip ext device name
    Signed-off-by: Catherine <tuhongj@cn.ibm.com>

--- scripts/backend/create_floating.sh
+++ scripts/backend/create_floating.sh
@@ -5,6 +5,7 @@ source ../cloudrc
 
 [ $# -lt 4 ] && echo "$0 <router> <ext_type> <ext_ip> <int_ip>" && exit -1
 
+ID=$1
 router=router-$1
 ext_type=$2
 ext_ip=${3%/*}
@@ -15,11 +16,11 @@ ip netns list | grep -q $router
 [ $? -ne 0 ] && exit 0
 
 if [ "$ext_type" = "public" ]; then
-    ext_dev=$(ip netns exec $router ip -o link show | cut -d: -f2 | cut -d@ -f1 | grep ext | xargs)
+    ext_dev=te-$ID
     ip netns exec $router iptables -t nat -I PREROUTING -d $ext_ip -i $ext_dev -j DNAT --to-destination $int_ip
     ip netns exec $router iptables -t nat -I POSTROUTING -s $int_ip ! -d 10.0.0.0/8 -o $ext_dev -j SNAT --to-source $ext_ip
 elif [ "$ext_type" = "private" ]; then
-    ext_dev=$(ip netns exec $router ip -o link show | cut -d: -f2 | cut -d@ -f1 | grep int | xargs)
+    ext_dev=ti-$ID
     ip netns exec $router iptables -t nat -I PREROUTING -d $ext_ip -i $ext_dev -j DNAT --to-destination $int_ip
     ip netns exec $router iptables -t nat -I POSTROUTING -s $int_ip -d 10.0.0.0/8 -o $ext_dev -j SNAT --to-source $ext_ip
 fi
