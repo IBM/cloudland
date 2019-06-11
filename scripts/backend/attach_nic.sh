463688de954d5cf58e832ef03338db08c4b18595
Catherine <tuhongj@cn.ibm.com>
2019-06-11 14:31:47 +0800

    Fix create sg chain param issue
    Signed-off-by: Catherine <tuhongj@cn.ibm.com>

--- scripts/backend/attach_nic.sh
+++ scripts/backend/attach_nic.sh
@@ -13,5 +13,5 @@ nic_name=tap$(echo $vm_mac | cut -d: -f4- | tr -d :)
 vm_br=br$vlan
 ./create_link.sh $vlan
 virsh attach-interface $vm_ID bridge $vm_br --model virtio --mac $vm_mac --config --target $nic_name
-./create_sg_chain.sh $vm_ip $vm_mac
+./create_sg_chain.sh $nic_name $vm_ip $vm_mac
 ./apply_sg_rule.sh $nic_name
