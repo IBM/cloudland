b74274c9946ce068a093dee8b5abd31a5f732e39
Spark.Liu <spark.liu@cn.ibm.com>
2019-07-04 14:56:42 +0800

    suport token midleware

--- web/clui/routes/routes_rest.go
+++ web/clui/routes/routes_rest.go
@@ -10,6 +10,7 @@ package routes
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -18,12 +19,17 @@ import (
 
 var (
 	resourceEndpoints = map[string]string{
-		"volume":   `/volume/v3`,
-		"compute":  `/compute/v2.1`,
-		"image":    `/image`,
-		"network":  `/v2.0/networks`,
-		"identity": `/identity`,
-		"subnet":   `/v2.0/subnets`,
+		"volume":        `/volume/v3`,
+		"compute":       `/compute/v2.1`,
+		"image":         `/image`,
+		"network":       `/v2.0/networks`,
+		"identity":      `/identity`,
+		"subnet":        `/v2.0/subnets`,
+		"identityToken": "/identity/v3/auth/tokens",
+	}
+	unAuthenResources = []string{
+		resourceEndpoints["identityToken"],
+		resourceEndpoints["identity"],
 	}
 )
 
@@ -44,9 +50,11 @@ func Rest() (m *macaron.Macaron) {
 			},
 		},
 	))
+	//token middleware
+	m.Use(TokenProcess)
 	//	m.Use(macaron.Renderer())
 	m.Get(resourceEndpoints["identity"], versionInstance.ListVersion)
-	m.Post("/identity/v3/auth/tokens", tokenInstance.IssueTokenByPasswd)
+	m.Post(resourceEndpoints["identityToken"], tokenInstance.IssueTokenByPasswd)
 	//neutron network api
 	m.Get(resourceEndpoints["network"], subnetInstance.ListNetworks)
 	m.Post(resourceEndpoints["network"], subnetInstance.CreateNetwork)
@@ -55,6 +63,26 @@ func Rest() (m *macaron.Macaron) {
 	m.Get(resourceEndpoints["subnet"], subnetInstance.ListSubnet)
 	m.Post(resourceEndpoints["subnet"], subnetInstance.CreateSubnet)
 	m.Delete(resourceEndpoints["subnet"]+`/:id`, subnetInstance.DeleteSubnet)
+
+	return
+}
+
+func TokenProcess(c *macaron.Context) {
+	for _, unAuthenResource := range unAuthenResources {
+		if unAuthenResource == c.Req.RequestURI {
+			return
+		}
+	}
+	if claims, err := ParseToken(c.Req.Header.Get("X-Auth-Token")); err != nil {
+		log.Println(err.Error())
+		c.Error(403, "Unauthenticatins")
+		return
+	} else {
+		log.Println(fmt.Sprintf("%+v", claims))
+		// TODO: check token's expire time
+		// TODO: save claims data to context  and check action and authority base on userID and orgID
+		c.Data["claims"] = claims
+	}
 
 	return
 }
