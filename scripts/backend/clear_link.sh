e45784aa18a40185756b9e5931f59379c2b0cd93
Catherine <tuhongj@cn.ibm.com>
2019-07-26 04:55:29 -0500

    Do not auto delete internal or external network
    Signed-off-by: Catherine <tuhongj@cn.ibm.com>

--- scripts/backend/clear_link.sh
+++ scripts/backend/clear_link.sh
@@ -7,10 +7,13 @@ source ../cloudrc
 
 vlan=$1
 force=$2
+[ -z "$force" ] && force=no
 vm_br=br$vlan
 slaves=$(ls -A /sys/devices/virtual/net/$vm_br/brif | grep -v v-)
 [ -n "$slaves" ] && exit 0
-[ "$force" = "no" -o "$vlan" = "$external_vlan" -o "$vlan" = "$internal_vlan" ] && exit 0
+if [ "$force" = "no" ]; then
+    [ "$vlan" = "$external_vlan" -o "$vlan" = "$internal_vlan" ] && exit 0
+fi
 nmcli connection down v-$vlan
 nmcli connection del v-$vlan
 nmcli connection down $vm_br
