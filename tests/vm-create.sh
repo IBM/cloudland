d01388f1a28e00ea061a0b11304c422e8e537f47
wenshilin <2439774702@qq.com>
2020-09-28 22:20:36 +0800

    Add files via upload
    Write some auto test cases for basic functions #144

---
+++ tests/vm-create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+qemu-img create -f qcow2 -b \
+/opt/cloudland/cache/image/image-1.qcow2 \
+/opt/cloudland/cache/image/image-2.qcow2
+
+virt-install \
+--name=image-2 \
+--vcpus=1 \
+--memory=1024 \
+--disk path=/opt/cloudland/cache/image/image-2.qcow2,bus=virtio,format=qcow2 \
+--network bridge=virbr0 \
+--import &
