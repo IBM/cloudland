10d08ebd3367854b0df72f33cbd1d5636bac8ddb
wenshilin <2439774702@qq.com>
2020-10-13 10:47:55 +0000

    auto test fuctions cases
    Signed-off-by: wenshilin <2439774702@qq.com>

--- testcases/create_floatingip.sh
+++ testcases/create_floatingip.sh
@@ -17,22 +17,22 @@ file=$path/cookie.txt
 
 #save cookie.txt
 echo "Save cookie!"
-curl -v -c cookie.txt -X POST -d "username=admin&password=passw0rd" --insecure https://169.61.25.50/login
+curl -v -c cookie.txt -X POST -d "username=admin&password=passw0rd" --insecure http://169.61.25.58/login
 
-echo "Create key!"
-curl -i -g -b cookie.txt -X POST -d "name=key" --insecure https://169.61.25.50/keys/confirm
+#echo "Create key!"
+#curl -i -g -b cookie.txt -X POST -d "name=key" --insecure http://169.61.25.58/keys/confirm
 
-echo "Create ziwang1!"
+#echo "Create ziwang!"
 #curl -i -g -b cookie.txt -X POST -d "name=ziwang&network=10.4.35.56&netmask=255.255.255.240&dhcp="yes"" --insecure http://169.61.25.58/subnets/new
 
-echo "Create wangguan!"
+#echo "Create wangguan!"
 #curl -i -g -b cookie.txt -X POST -d "name=wangguan&subnets=14" --insecure http://169.61.25.58/gateways/new
 
-echo "Create vm instance!"
+#echo "Create vm instance!"
 #curl -i -g -b cookie.txt -X POST -d "hostname=inst&hyper=-1&count=1&image=1&flavor=1&primary=14" --insecure http://169.61.25.58/instances/new -H "Accept:application/json"
 
 echo "Create floatingip!"
-#curl -i -g -b cookie.txt -X POST -d "instance=16&ftype="public"" --insecure http://169.61.25.58/floatingips/new
+curl -i -g -b cookie.txt -X POST -d "instance=16&ftype="public"" --insecure http://169.61.25.58/floatingips/new
 
 if [ -f "$file" ]; then
    echo "delete file!"
