eeafd9a69107a78bb15e39cabeea8276a2c22a1d
jinlings <jinlings@cn.ibm.com>
2021-01-14 22:25:03 +0800

    remove log

--- scripts/kvm/rm_fdb.sh
+++ scripts/kvm/rm_fdb.sh
@@ -4,6 +4,4 @@ cd `dirname $0`
 source ../cloudrc
 [ $# -lt 1 ] && die "$0 <mac_addr>"
 mac_addr=$1
-echo "starting to remove record from fdb" >> /tmp/del_fdb.log
 sudo /usr/sbin/bridge fdb del $mac_addr dev $zlayer2_interface
-echo "${mac_addr} records removed from fdb" >> /tmp/del_fdb.log
