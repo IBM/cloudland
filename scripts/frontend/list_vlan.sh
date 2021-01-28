3ffed35d2255587377c580c68cfdb35a9d789c79
Catherine <tuhongj@cn.ibm.com>
2021-01-28 07:33:42 -0500

    Adjust some parameters for redhat hypervisor
    Signed-off-by: Catherine <tuhongj@cn.ibm.com>

--- scripts/frontend/list_vlan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd `dirname $0`
-source ../cloudrc
-
-[ $# -lt 1 ] && echo "$0 <user>" && exit -1
-
-owner=$1
-sql_exec "select vlan, network, netmask, gateway, start_address, end_address, owner from network where owner='$owner' or shared='true' COLLATE NOCASE"
