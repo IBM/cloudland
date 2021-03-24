dc0d7f715cb0d5842c07eac85cabffe453fdc151
jinlings <jinlings@cn.ibm.com>
2021-03-24 02:27:20 -0400

    fix root
    Signed-off-by: jinlings <jinlings@cn.ibm.com>

--- autocheck.sh
+++ autocheck.sh
@@ -14,6 +14,7 @@ checkpr(){
   sudo git clone --branch=$BRANCHNAME https://github.com/$PRSLUG.git
   sudo touch ./cloudland/web/clui/public/test_status
   sudo chown cland:cland ./cloudland/web/clui/public/test_status
+  ls -lrt ./cloudland/web/clui/public/test_status
   sudo echo "PENDING" > ./cloudland/web/clui/public/test_status
   cd /opt/cloudland/deploy/
   ./deploy.sh
