6e3cc491ecc05c3a387163076131f56b10636eb5
jnanbj <jnanbj@cn.ibm.com>
2021-08-20 15:52:56 +0800

    fix subnet return bug2
    Signed-off-by: jnanbj <jnanbj@cn.ibm.com>

--- web/clui/routes/subnet.go
+++ web/clui/routes/subnet.go
@@ -442,7 +442,7 @@ func (a *SubnetAdmin) Delete(ctx context.Context, id int64) (err error) {
 		return
 	}
 	if subnet.Router > 0 {
-		err = fmt.Errorf("Subnet belongs to a gateway")
+		err = fmt.Errorf("Subnet belongs to a gateway.")
 		log.Println("Subnet belongs to a gateway", err)
 		return
 	}
@@ -718,7 +718,7 @@ func (v *APISubnetView) Delete(c *macaron.Context, store session.Store) (err err
 	err = subnetAdmin.Delete(c.Req.Context(), int64(subnetID))
 	if err != nil {
 		c.JSON(500, map[string]interface{}{
-			"ErrorMsg": "Failed to delete subnet.",
+			"ErrorMsg": "Failed to delete subnet."+err.Error(),
 		})
 		return
 	}
