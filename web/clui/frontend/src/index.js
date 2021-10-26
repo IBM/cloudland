0316daba7aebb550d34c2db01071282f957f3ab0
Catherine <tuhongj@cn.ibm.com>
2021-10-25 21:38:32 -0400

    delete console
    Signed-off-by: Catherine <tuhongj@cn.ibm.com>

--- web/clui/frontend/src/index.js
+++ web/clui/frontend/src/index.js
@@ -43,6 +43,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 React.Component.prototype.$config = window.config;
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
