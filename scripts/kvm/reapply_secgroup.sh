cc7b2814ed35ff7dc9294b3e80b7d25009af8dc6
Catherine <tuhongj@cn.ibm.com>
2025-01-16 22:33:48 +0800

    fix reapply secgroup allow spoofing issue
    Signed-off-by: Catherine <tuhongj@cn.ibm.com>
    Signed-off-by: bruce <bruce@raksmart.com>

--- scripts/kvm/reapply_secgroup.sh
+++ scripts/kvm/reapply_secgroup.sh
@@ -3,11 +3,13 @@
 cd `dirname $0`
 source ../cloudrc
 
-[ $# -lt 2 ] && echo "$0 <vm_ip> <vm_mac>" && exit -1
+[ $# -lt 3 ] && echo "$0 <vm_ip> <vm_mac> <allow_spoofing>" && exit -1
 
 vm_ip=${1%%/*}
 vm_mac=$2
+allow_spoofing=$3
+
 nic_name=tap$(echo $vm_mac | cut -d: -f4- | tr -d :)
-./clear_sg_chain.sh $nic_name
-./create_sg_chain.sh $nic_name $vm_ip $vm_mac
-./apply_sg_rule.sh $nic_name
+./clear_sg_chain.sh "$nic_name"
+./create_sg_chain.sh "$nic_name" "$vm_ip" "$vm_mac" "$allow_spoofing"
+./apply_sg_rule.sh "$nic_name"
