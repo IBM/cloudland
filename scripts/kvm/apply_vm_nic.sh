730aa9606c0bf5947c33ee5f9a89b5ab9a9e8e53
Catherine <tuhongj@cn.ibm.com>
2025-02-19 14:05:40 +0800

    fix outbound param and add udev settle
    Signed-off-by: Catherine <tuhongj@cn.ibm.com>

--- scripts/kvm/apply_vm_nic.sh
+++ scripts/kvm/apply_vm_nic.sh
@@ -24,6 +24,7 @@ if [ $? -ne 0 ]; then
     timeout_virsh attach-interface $vm_ID bridge $vm_br --model virtio --mac $vm_mac --target $nic_name --live
     timeout_virsh attach-interface $vm_ID bridge $vm_br --model virtio --mac $vm_mac --target $nic_name --config
 fi
+udevadm settle
 ./set_nic_speed.sh "$ID" "$nic_name" "$inbound" "$outbound"
 ./create_sg_chain.sh "$nic_name" "$vm_ip" "$vm_mac" "$allow_spoofing" >/dev/null 2>&1
 ./apply_sg_rule.sh "$nic_name"
