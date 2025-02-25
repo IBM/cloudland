175637ff24f206bd18952791846fb2256e59d10b
bryan <bryan@raksmart.com>
2025-02-25 17:40:13 +0800

    PET-376: fix cannot apply ssh port change on ubuntu 2404
    Signed-off-by: bryan <bryan@raksmart.com>

--- scripts/kvm/build_meta.sh
+++ scripts/kvm/build_meta.sh
@@ -113,7 +113,10 @@ if [ -n "${login_port}" ] && [ "${login_port}" != "22" ] && [ ${login_port} -gt 
         echo \
 'runcmd:\n'\
 '    - sed -i \"s/^#Port .*/Port '${login_port}'/\" /etc/ssh/sshd_config\n'\
-'    - systemctl restart sshd\n'
+'    - sed -i \"s/^Port .*/Port '${login_port}'/\" /etc/ssh/sshd_config\n'\
+'    - systemctl daemon-reload\n'\
+'    - systemctl restart sshd\n'\
+'    - systemctl restart ssh.socket\n'
     )
 fi
 
