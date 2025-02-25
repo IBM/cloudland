9152146cafce560fe848cc42dc76484093a12030
Catherine <tuhongj@cn.ibm.com>
2025-02-25 13:10:00 +0800

    download image via system router
    Signed-off-by: Catherine <tuhongj@cn.ibm.com>

--- deploy/roles/cland/files/firewall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-iptables -D INPUT -p tcp -m state --state NEW -m tcp --dport 5006 -j ACCEPT
-iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 5006 -j ACCEPT
-
-/sbin/iptables-save -c > /etc/iptables.rules
