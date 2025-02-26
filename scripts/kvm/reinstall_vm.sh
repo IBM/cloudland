fd5f3f08b52a3239367db0820ea66561a1f88e1c
Jack <jack@raksmart.com>
2025-02-27 00:59:53 +0800

    add reinstall_vm dispather

--- scripts/kvm/reinstall_vm.sh
+++ scripts/kvm/reinstall_vm.sh
@@ -14,6 +14,7 @@ old_volume_id=$5
 vm_cpu=$6
 vm_mem=$7
 disk_size=$8
+state=error
 
 vm_xml=$xml_dir/$vm_ID/${vm_ID}.xml
 mv $vm_xml $vm_xml-$(date +'%s.%N')
@@ -27,7 +28,7 @@ if [ -z "$wds_address" ]; then
         vm_img=$image_dir/$vm_ID.disk
         if [ ! -s "$image_cache/$img_name" ]; then
             echo "Image is not available!"
-            echo "|:-COMMAND-:| `basename $0` '$ID' '$state' '$SCI_CLIENT_ID' 'image $img_name not available!'"
+            echo "|:-COMMAND-:| `basename $0` '$ID' '$state' '$SCI_CLIENT_ID' '$vol_ID' 'image $img_name not available!'"
             exit -1
         fi
         format=$(qemu-img info $image_cache/$img_name | grep 'file format' | cut -d' ' -f3)
@@ -35,7 +36,7 @@ if [ -z "$wds_address" ]; then
         result=$(eval "$cmd")
         vsize=$(qemu-img info $vm_img | grep 'virtual size:' | cut -d' ' -f5 | tr -d '(')
         if [ "$vsize" -gt "$fsize" ]; then
-            echo "|:-COMMAND-:| `basename $0` '$ID' '$vm_stat' '$SCI_CLIENT_ID' 'flavor is smaller than image size'"
+            echo "|:-COMMAND-:| `basename $0` '$ID' '$vm_stat' '$SCI_CLIENT_ID' '$vol_ID' 'flavor is smaller than image size'"
             exit -1
         fi
         qemu-img resize -q $vm_img "${disk_size}G" &> /dev/null
@@ -54,11 +55,11 @@ else
     snapshot_name=${image}-${snapshot}
     snapshot_id=$(wds_curl GET "api/v2/sync/block/snaps?name=$snapshot_name" | jq -r '.snaps[0].id')
     if [ -z "$snapshot_id" -o "$snapshot_id" = null ]; then
-	image_volume_id=$(wds_curl GET "api/v2/sync/block/volumes?name=$image" | jq -r '.volumes[0].id')
-	snapshot_ret=$(wds_curl POST "api/v2/sync/block/snaps" "{\"name\": \"$snapshot_name\", \"description\": \"$snapshot_name\", \"volume_id\": \"$image_volume_id\"}")
+	      image_volume_id=$(wds_curl GET "api/v2/sync/block/volumes?name=$image" | jq -r '.volumes[0].id')
+	      snapshot_ret=$(wds_curl POST "api/v2/sync/block/snaps" "{\"name\": \"$snapshot_name\", \"description\": \"$snapshot_name\", \"volume_id\": \"$image_volume_id\"}")
         snapshot_id=$(wds_curl GET "api/v2/sync/block/snaps?name=$snapshot_name" | jq -r '.snaps[0].id')
         if [ -z "$snapshot_id" -o "$snapshot_id" = null ]; then
-            echo "|:-COMMAND-:| `basename $0` '$ID' '$state' '$SCI_CLIENT_ID' 'failed to create image snapshot, $snapshot_ret'"
+            echo "|:-COMMAND-:| `basename $0` '$ID' '$state' '$SCI_CLIENT_ID' '$vol_ID' 'failed to create image snapshot, $snapshot_ret'"
             exit -1
         fi
         wds_curl DELETE "api/v2/sync/block/snaps/$image-$(($snapshot-1))?force=false"
@@ -71,13 +72,13 @@ else
     volume_ret=$(wds_curl POST "api/v2/sync/block/snaps/$snapshot_id/clone" "{\"name\": \"$vhost_name\"}")
     volume_id=$(echo $volume_ret | jq -r .id)
     if [ -z "$volume_id" -o "$volume_id" = null ]; then
-        echo "|:-COMMAND-:| `basename $0` '$ID' '$state' '$SCI_CLIENT_ID' 'failed to create boot volume based on snapshot $snapshot_name, $volume_ret!'"
+        echo "|:-COMMAND-:| `basename $0` '$ID' '$state' '$SCI_CLIENT_ID' '$vol_ID' 'failed to create boot volume based on snapshot $snapshot_name, $volume_ret!'"
         exit -1
     fi
     expand_ret=$(wds_curl PUT "api/v2/sync/block/volumes/$volume_id/expand" "{\"size\": $fsize}")
     ret_code=$(echo $expand_ret | jq -r .ret_code)
     if [ "$ret_code" != "0" ]; then
-        echo "|:-COMMAND-:| `basename $0` '$ID' '$state' '$SCI_CLIENT_ID' 'failed to expand boot volume to size $fsize, $expand_ret'"
+        echo "|:-COMMAND-:| `basename $0` '$ID' '$state' '$SCI_CLIENT_ID' '$vol_ID' 'failed to expand boot volume to size $fsize, $expand_ret'"
         exit -1
     fi
     vhost_ret=$(wds_curl POST "api/v2/sync/block/vhost" "{\"name\": \"$vhost_name\"}")
@@ -85,7 +86,7 @@ else
     uss_ret=$(wds_curl PUT "api/v2/sync/block/vhost/bind_uss" "{\"vhost_id\": \"$vhost_id\", \"uss_gw_id\": \"$uss_id\", \"lun_id\": \"$volume_id\", \"is_snapshot\": false}")
     ret_code=$(echo $uss_ret | jq -r .ret_code)
     if [ "$ret_code" != "0" ]; then
-        echo "|:-COMMAND-:| `basename $0` '$ID' '$state' '$SCI_CLIENT_ID' 'failed to create wds vhost for boot volume, $vhost_ret, $uss_ret!'"
+        echo "|:-COMMAND-:| `basename $0` '$ID' '$state' '$SCI_CLIENT_ID' '$vol_ID' 'failed to create wds vhost for boot volume, $vhost_ret, $uss_ret!'"
         exit -1
     fi
     echo "|:-COMMAND-:| create_volume_wds_vhost '$vol_ID' 'attached' 'wds_vhost://$wds_pool_id/$volume_id'"
