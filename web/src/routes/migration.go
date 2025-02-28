7d9408efc3f7fad9f20255fdabbfb4b9eb464faf
Catherine <tuhongj@cn.ibm.com>
2025-02-28 15:46:17 +0800

    implement migration functions
    Signed-off-by: Catherine <tuhongj@cn.ibm.com>

--- web/src/routes/migration.go
+++ web/src/routes/migration.go
@@ -24,166 +24,165 @@ import (
 )
 
 var (
-	MigrationAdmin = &MigrationAdmin{}
-	MigrationView  = &MigrationView{}
+	migrationAdmin = &MigrationAdmin{}
+	migrationView  = &MigrationView{}
 )
 
 type MigrationAdmin struct{}
 type MigrationView struct{}
 
-func FileExist(filename string) bool {
-	_, err := os.Lstat(filename)
-	return !os.IsNotExist(err)
-}
-
-func (a *MigrationAdmin) Create(ctx context.Context, osCode, name, osVersion, virtType, userName, url, architecture string, qaEnabled bool, instID int64) (Migration *model.Migration, err error) {
-	logger.Debugf("Creating Migration %s %s %s %s %s %s %s %t %d", osCode, name, osVersion, virtType, userName, url, architecture, qaEnabled, instID)
+func (a *MigrationAdmin) Create(ctx context.Context, name, instance *model.Instance, force bool, toHyper *model.Hyper) (migration *model.Migration, err error) {
+	logger.Debugf("Start migrating %s for instance %d from %d to %d", name, instance.ID, instance.Hyper, toHyper)
 	memberShip := GetMemberShip(ctx)
+	permit = memberShip.CheckPermission(model.Admin)
+	if !permit {
+		logger.Error("Not authorized for this operation")
+		err = fmt.Errorf("Not authorized for this operation")
+		return
+	}
+	if instance.Hyper == toHyper.Hostid {
+		logger.Error("No need to migrate if source and target hypervisors are the same")
+		err = fmt.Errorf("No need to migrate if source and target hypervisors are the same")
+		return
+	}
+	if toHyper.Status != 1 {
+		logger.Error("Target hypervisors is not available")
+		err = fmt.Errorf("Target hypervisor is not available")
+		return
+	}
 	ctx, db, newTransaction := StartTransaction(ctx)
 	defer func() {
 		if newTransaction {
 			EndTransaction(ctx, err)
 		}
 	}()
-	var instance *model.Instance
-	if instID > 0 {
-		instance = &model.Instance{Model: model.Model{ID: instID}}
-		err = db.Preload("Migration").Preload("Volumes").Take(instance).Error
-		if err != nil {
-			logger.Error("DB failed to query instance", err)
-			return
-		}
-		if instance.Status != "shut_off" {
-			err = fmt.Errorf("instance [%s] is running, shut it down first before capturing", instance.Hostname)
-			logger.Error(err)
-			return
-		}
-		Migration = instance.Migration.Clone()
-		Migration.Model = model.Model{Creater: memberShip.UserID}
-		Migration.Owner = memberShip.OrgID
-		Migration.Name = name
-		Migration.Status = "creating"
-		Migration.CaptureFromInstanceID = instance.ID
-		Migration.CaptureFromInstance = instance
-	} else {
-		Migration = &model.Migration{
-			Model:        model.Model{Creater: memberShip.UserID},
-			Owner:        memberShip.OrgID,
-			OsVersion:    osVersion,
-			VirtType:     virtType,
-			UserName:     userName,
-			Name:         name,
-			OSCode:       osCode,
-			Status:       "creating",
-			Architecture: architecture,
-			QAEnabled:    qaEnabled,
-		}
-	}
-	logger.Debugf("Creating Migration %+v", Migration)
-	err = db.Create(Migration).Error
+	hyper := &model.Hyper{Hostid: instance.Hyper)
+	err = db.Where(hyper).Take(hyper).Error
 	if err != nil {
-		logger.Error("DB create Migration failed, %v", err)
+		logger.Error("Failed to query hyper", err)
+		return
 	}
-	prefix := strings.Split(Migration.UUID, "-")[0]
-	control := "inter="
-	command := fmt.Sprintf("/opt/cloudland/scripts/backend/create_Migration.sh '%d' '%s' '%s'", Migration.ID, prefix, url)
-	if instID > 0 {
-		bootVolumeUUID := ""
-		if instance.Volumes != nil {
-			for _, volume := range instance.Volumes {
-				if volume.Booting {
-					bootVolumeUUID = volume.GetOriginVolumeID()
-					break
-				}
-			}
+	task1 := &model.Task{
+		Name: "Migration_Step1",
+		Summary: "Processing resources on source hypervisor",
+	}
+	shutdown := false
+	if force {
+		if fromHyper.Status == 1 {
+			task1.Status = "in_progress"
+			shutdown = true
+		} else {
+			task1.Status = "not_doing"
 		}
-		control = fmt.Sprintf("inter=%d", instance.Hyper)
-		command = fmt.Sprintf("/opt/cloudland/scripts/backend/capture_Migration.sh '%d' '%s' '%d' '%s'", Migration.ID, prefix, instance.ID, bootVolumeUUID)
 	}
+	migration = &model.Migration{
+		Model:        model.Model{Creater: memberShip.UserID},
+		Name:         name,
+		Force:        force,
+		FromHyper:    instance.Hyper,
+		ToHyper:      toHyper.Hostid,
+		Phases:       []*Tasks{task1},
+	}
+	logger.Debugf("Creating migration %+v", migration)
+	err = db.Create(migration).Error
+	if err != nil {
+		logger.Error("DB create migration failed, %v", err)
+	}
+	if shutdown {
+		control := fmt.Sprintf("inter=%d", instance.Hyper)
+		command := fmt.Sprintf("/opt/cloudland/scripts/backend/action_vm.sh '%d' '%s'", instance.ID, "shutdown")
+		err = HyperExecute(ctx, control, command)
+		if err != nil {
+			logger.Error("Shutting down instance failed", err)
+			return
+		}
+	}
+	command := fmt.Sprintf("/opt/cloudland/scripts/backend/create_migration.sh '%d' '%s' '%s'", migration.ID, prefix, url)
+	control = fmt.Sprintf("inter=%d", instance.Hyper)
 	err = HyperExecute(ctx, control, command)
 	if err != nil {
-		logger.Error("Create Migration command execution failed", err)
+		logger.Error("Create migration command execution failed", err)
 		return
 	}
 	return
 }
 
-func (a *MigrationAdmin) GetMigrationByUUID(ctx context.Context, uuID string) (Migration *model.Migration, err error) {
+func (a *MigrationAdmin) GetMigrationByUUID(ctx context.Context, uuID string) (migration *model.Migration, err error) {
 	db := DB()
-	Migration = &model.Migration{}
-	err = db.Where("uuid = ?", uuID).Take(Migration).Error
+	migration = &model.Migration{}
+	err = db.Where("uuid = ?", uuID).Take(migration).Error
 	if err != nil {
-		logger.Error("Failed to query Migration, %v", err)
+		logger.Error("Failed to query migration, %v", err)
 		return
 	}
 	memberShip := GetMemberShip(ctx)
 	permit := memberShip.CheckPermission(model.Reader)
 	if !permit {
-		logger.Error("Not authorized to get Migration")
+		logger.Error("Not authorized to get migration")
 		err = fmt.Errorf("Not authorized")
 		return
 	}
 	return
 }
 
-func (a *MigrationAdmin) GetMigrationByName(ctx context.Context, name string) (Migration *model.Migration, err error) {
+func (a *MigrationAdmin) GetMigrationByName(ctx context.Context, name string) (migration *model.Migration, err error) {
 	db := DB()
-	Migration = &model.Migration{}
-	err = db.Where("name = ?", name).Take(Migration).Error
+	migration = &model.Migration{}
+	err = db.Where("name = ?", name).Take(migration).Error
 	if err != nil {
-		logger.Error("Failed to query Migration, %v", err)
+		logger.Error("Failed to query migration, %v", err)
 		return
 	}
 	memberShip := GetMemberShip(ctx)
 	permit := memberShip.CheckPermission(model.Reader)
 	if !permit {
-		logger.Error("Not authorized to get Migration")
+		logger.Error("Not authorized to get migration")
 		err = fmt.Errorf("Not authorized")
 		return
 	}
 	return
 }
 
-func (a *MigrationAdmin) Get(ctx context.Context, id int64) (Migration *model.Migration, err error) {
+func (a *MigrationAdmin) Get(ctx context.Context, id int64) (migration *model.Migration, err error) {
 	if id <= 0 {
-		err = fmt.Errorf("Invalid Migration ID: %d", id)
+		err = fmt.Errorf("Invalid migration ID: %d", id)
 		logger.Error(err)
 		return
 	}
 	db := DB()
-	Migration = &model.Migration{Model: model.Model{ID: id}}
-	err = db.Take(Migration).Error
+	migration = &model.Migration{Model: model.Model{ID: id}}
+	err = db.Take(migration).Error
 	if err != nil {
-		logger.Error("DB failed to query Migration, %v", err)
+		logger.Error("DB failed to query migration, %v", err)
 		return
 	}
 	memberShip := GetMemberShip(ctx)
 	permit := memberShip.CheckPermission(model.Reader)
 	if !permit {
-		logger.Error("Not authorized to get Migration")
+		logger.Error("Not authorized to get migration")
 		err = fmt.Errorf("Not authorized")
 		return
 	}
 	return
 }
 
-func (a *MigrationAdmin) GetMigration(ctx context.Context, reference *BaseReference) (Migration *model.Migration, err error) {
+func (a *MigrationAdmin) GetMigration(ctx context.Context, reference *BaseReference) (migration *model.Migration, err error) {
 	if reference == nil || (reference.ID == "" && reference.Name == "") {
 		err = fmt.Errorf("Migration base reference must be provided with either uuid or name")
 		return
 	}
 	if reference.ID != "" {
-		Migration, err = a.GetMigrationByUUID(ctx, reference.ID)
+		migration, err = a.GetMigrationByUUID(ctx, reference.ID)
 		return
 	}
 	if reference.Name != "" {
-		Migration, err = a.GetMigrationByName(ctx, reference.Name)
+		migration, err = a.GetMigrationByName(ctx, reference.Name)
 		return
 	}
 	return
 }
 
-func (a *MigrationAdmin) Delete(ctx context.Context, Migration *model.Migration) (err error) {
+func (a *MigrationAdmin) Delete(ctx context.Context, migration *model.Migration) (err error) {
 	ctx, db, newTransaction := StartTransaction(ctx)
 	defer func() {
 		if newTransaction {
@@ -191,40 +190,40 @@ func (a *MigrationAdmin) Delete(ctx context.Context, Migration *model.Migration)
 		}
 	}()
 	memberShip := GetMemberShip(ctx)
-	permit := memberShip.ValidateOwner(model.Writer, Migration.Owner)
+	permit := memberShip.ValidateOwner(model.Writer, migration.Owner)
 	if !permit {
-		logger.Error("Not authorized to delete Migration")
+		logger.Error("Not authorized to delete migration")
 		err = fmt.Errorf("Not authorized")
 		return
 	}
 	refCount := 0
-	err = db.Model(&model.Instance{}).Where("Migration_id = ?", Migration.ID).Count(&refCount).Error
+	err = db.Model(&model.Instance{}).Where("migration_id = ?", migration.ID).Count(&refCount).Error
 	if err != nil {
-		logger.Error("Failed to count the number of instances using the Migration", err)
+		logger.Error("Failed to count the number of instances using the migration", err)
 		return
 	}
 	if refCount > 0 {
 		logger.Error("Migration can not be deleted if there are instances using it")
-		err = fmt.Errorf("The Migration can not be deleted if there are instances using it")
+		err = fmt.Errorf("The migration can not be deleted if there are instances using it")
 		return
 	}
-	if Migration.Status == "available" {
-		prefix := strings.Split(Migration.UUID, "-")[0]
+	if migration.Status == "available" {
+		prefix := strings.Split(migration.UUID, "-")[0]
 		control := "inter="
-		command := fmt.Sprintf("/opt/cloudland/scripts/backend/clear_Migration.sh '%d' '%s' '%s'", Migration.ID, prefix, Migration.Format)
+		command := fmt.Sprintf("/opt/cloudland/scripts/backend/clear_migration.sh '%d' '%s' '%s'", migration.ID, prefix, migration.Format)
 		err = HyperExecute(ctx, control, command)
 		if err != nil {
-			logger.Error("Clear Migration command execution failed", err)
+			logger.Error("Clear migration command execution failed", err)
 			return
 		}
 	}
-	if err = db.Delete(Migration).Error; err != nil {
+	if err = db.Delete(migration).Error; err != nil {
 		return
 	}
 	return
 }
 
-func (a *MigrationAdmin) List(offset, limit int64, order, query string) (total int64, Migrations []*model.Migration, err error) {
+func (a *MigrationAdmin) List(offset, limit int64, order, query string) (total int64, migrations []*model.Migration, err error) {
 	db := DB()
 	if limit == 0 {
 		limit = 16
@@ -237,12 +236,12 @@ func (a *MigrationAdmin) List(offset, limit int64, order, query string) (total i
 	if query != "" {
 		query = fmt.Sprintf("name like '%%%s%%'", query)
 	}
-	Migrations = []*model.Migration{}
+	migrations = []*model.Migration{}
 	if err = db.Model(&model.Migration{}).Where(query).Count(&total).Error; err != nil {
 		return
 	}
 	db = dbs.Sortby(db.Offset(offset).Limit(limit), order)
-	if err = db.Where(query).Find(&Migrations).Error; err != nil {
+	if err = db.Where(query).Find(&migrations).Error; err != nil {
 		return
 	}
 
@@ -268,18 +267,18 @@ func (v *MigrationView) List(c *macaron.Context, store session.Store) {
 		order = "-created_at"
 	}
 	query := c.QueryTrim("q")
-	total, Migrations, err := MigrationAdmin.List(offset, limit, order, query)
+	total, migrations, err := migrationAdmin.List(offset, limit, order, query)
 	if err != nil {
 		c.Data["ErrorMsg"] = err.Error()
 		c.Error(http.StatusInternalServerError)
 		return
 	}
 	pages := GetPages(total, limit)
-	c.Data["Migrations"] = Migrations
+	c.Data["Migrations"] = migrations
 	c.Data["Total"] = total
 	c.Data["Pages"] = pages
 	c.Data["Query"] = query
-	c.HTML(200, "Migrations")
+	c.HTML(200, "migrations")
 }
 
 func (v *MigrationView) Delete(c *macaron.Context, store session.Store) (err error) {
@@ -290,26 +289,26 @@ func (v *MigrationView) Delete(c *macaron.Context, store session.Store) (err err
 		c.Error(http.StatusBadRequest)
 		return
 	}
-	MigrationID, err := strconv.Atoi(id)
+	migrationID, err := strconv.Atoi(id)
 	if err != nil {
 		c.Data["ErrorMsg"] = err.Error()
 		c.Error(http.StatusBadRequest)
 		return
 	}
-	Migration, err := MigrationAdmin.Get(ctx, int64(MigrationID))
+	migration, err := migrationAdmin.Get(ctx, int64(migrationID))
 	if err != nil {
 		c.Data["ErrorMsg"] = err.Error()
 		c.Error(http.StatusBadRequest)
 		return
 	}
-	err = MigrationAdmin.Delete(ctx, Migration)
+	err = migrationAdmin.Delete(ctx, migration)
 	if err != nil {
 		c.Data["ErrorMsg"] = err.Error()
 		c.Error(http.StatusBadRequest)
 		return
 	}
 	c.JSON(200, map[string]interface{}{
-		"redirect": "Migrations",
+		"redirect": "migrations",
 	})
 	return
 }
@@ -330,7 +329,7 @@ func (v *MigrationView) New(c *macaron.Context, store session.Store) {
 		return
 	}
 	c.Data["Instances"] = instances
-	c.HTML(200, "Migrations_new")
+	c.HTML(200, "migrations_new")
 }
 
 func (v *MigrationView) Create(c *macaron.Context, store session.Store) {
@@ -342,7 +341,7 @@ func (v *MigrationView) Create(c *macaron.Context, store session.Store) {
 		c.HTML(http.StatusBadRequest, "error")
 		return
 	}
-	redirectTo := "../Migrations"
+	redirectTo := "../migrations"
 	osCode := c.QueryTrim("osCode")
 	name := c.QueryTrim("name")
 	url := c.QueryTrim("url")
@@ -352,9 +351,9 @@ func (v *MigrationView) Create(c *macaron.Context, store session.Store) {
 	userName := c.QueryTrim("userName")
 	qaEnabled := true
 	architecture := "x86_64"
-	_, err := MigrationAdmin.Create(c.Req.Context(), osCode, name, osVersion, virtType, userName, url, architecture, qaEnabled, instance)
+	_, err := migrationAdmin.Create(c.Req.Context(), osCode, name, osVersion, virtType, userName, url, architecture, qaEnabled, instance)
 	if err != nil {
-		logger.Error("Create Migration failed", err)
+		logger.Error("Create migration failed", err)
 		c.Data["ErrorMsg"] = err.Error()
 		c.HTML(http.StatusBadRequest, "error")
 		return
