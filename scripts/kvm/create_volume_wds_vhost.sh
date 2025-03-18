a9799ee5f04927e047fd97ca4a3d77396e263484
Jack <jack@raksmart.com>
2025-03-18 18:01:09 +0800

    remove burst payload from the POST volume api
    Signed-off-by: Jack <jack@raksmart.com>

--- scripts/kvm/create_volume_wds_vhost.sh
+++ scripts/kvm/create_volume_wds_vhost.sh
@@ -35,7 +35,7 @@ state="creating"
 let size=$size*1024*1024*1024 # GB to Bytes
 # fix wds said: "The volume name cannot start with a number"
 vol_WDS_NAME="vol-$vol_ID-$vol_UUID"
-result=$(wds_curl "POST" "/api/v2/sync/block/volumes" "{\"phy_pool_id\": \"$pool_ID\", \"name\": \"$vol_WDS_NAME\", \"volume_size\": $size, \"qos\":{\"iops_limit\": $iops_limit, \"iops_burst\": $iops_burst, \"bps_limit\": $bps_limit, \"bps_burst\": $bps_burst}}")
+result=$(wds_curl "POST" "/api/v2/sync/block/volumes" "{\"phy_pool_id\": \"$pool_ID\", \"name\": \"$vol_WDS_NAME\", \"volume_size\": $size, \"qos\":{\"iops_limit\": $iops_limit, \"bps_limit\": $bps_limit}}")
 ret_code=$(echo $result | jq -r .ret_code)
 if [ "$ret_code" != "0" ]; then
     echo "|:-COMMAND-:| $(basename $0) '$vol_ID' 'error' '' 'failed to create volume'"
